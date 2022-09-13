3112e6f84db4b3cac643d5cc15473577b60a6f7f
gumelar <gumelar.wicaksono6@gmail.com>
2022-09-13 15:50:07 +0700

    conditional variabel

---
+++ conditional-variabel/script.js
@@ -0,0 +1,40 @@
+let bulan = 3;
+
+let lulus = bulan == 4 ? 'Anda lulus' : 'maaf anda belum lulus';
+
+console.log(lulus);
+
+const hutang = 6;
+let lunas = hutang == 12 && 'hutang anda sudah lunas ';
+console.log(lunas);
+let blunas = hutang == 12 || 'hutang anda belom lunas ';
+console.log(blunas);
+
+// map and filter
+let NaamaSiswa = [
+  {
+    nama: 'putri',
+    namab: 'andini',
+    alamat: 'kp alur',
+  },
+  {
+    nama: 'nadia',
+    namab: 'alaska',
+    alamat: 'kp alur',
+  },
+  {
+    nama: 'alya',
+    namab: 'nurini',
+    alamat: 'ciranji',
+  },
+  {
+    nama: 'ralia',
+    namab: 'azahra',
+    alamat: 'cikalong',
+  },
+];
+
+let n = NaamaSiswa.map((n) => n.nama + ' ' + n.namab);
+console.log(n);
+let a = NaamaSiswa.filter((a) => a.alamat == 'kp alur');
+console.log(a);
